56fab048c6f4ee235ff634b6333cdc0ec4d40b94
Erik Edrosa <erik.edrosa@gmail.com>
2013-05-26 23:20:34 -0400

    Added file storage API

---
+++ src/server/storage/file.js
@@ -0,0 +1,13 @@
+/*
+ * Basic API for storing and retrieving posts in simple files
+ */
+function storeMessage(message, user) {
+
+}
+
+function getMessages() {
+
+}
+
+exports.storeMessage = storeMessage;
+exports.getMessages = getMessages;
