4a21dbc01410ccbf7647545601a17822196439f7
Erik Edrosa <erik.edrosa@gmail.com>
2013-06-30 00:26:26 -0400

    Handle HTTP OPTIONS method

--- src/server/router.js
+++ src/server/router.js
@@ -9,6 +9,19 @@ function route(boards, pathname, request, response) {
             case 'POST':
                 board.postMessage(request, response);
                 break;
+            case 'OPTIONS':
+                response.writeHead(200, {
+                      "Access-Control-Allow-Origin": "*",
+                      "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS",
+                      "Access-Control-Allow-Credentials": false,
+                      "Access-Control-Max-Age": '86400',
+                      "Access-Control-Allow-Headers": "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+                });
+               response.end(); 
+            default:
+                response.writeHead(405, {"Content-Type": "text/plain"});
+                response.write("405 Method not allowed");
+                response.end();
         }
     } else {
         response.writeHead(404, {"Content-Type": "text/plain"});
